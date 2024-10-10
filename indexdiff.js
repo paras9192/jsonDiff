235bab48ccb6ae221427609256b77e5b57a397a3
Paras Lohia <81807209+paras9192@users.noreply.github.com>
2024-10-10 17:06:59 +0530

    diff 2

--- indexdiff.js
+++ indexdiff.js
@@ -1,14 +1,12 @@
-const data =
-{
-    "name": "paras",
-    "summary": "South America (Spanish: América del Sur, Sudamérica or  \nSuramérica; Portuguese: América do Sul; Quechua and Aymara:  \nUrin Awya Yala; Guarani: Ñembyamérika; Dutch: Zuid-Amerika;  \nFrench: Amérique du Sud) is a continent situated in the  \nWestern Hemisphere, mostly in the Southern Hemisphere, with  \na relatively small portion in the Northern Hemisphere.  \nThe continent is also considered a subcontinent of the  \nAmericas.[2][3] It is bordered on the west by the Pacific  \nOcean and on the north and east by the Atlantic Ocean;  \nNorth America and the Caribbean Sea lie to the northwest.  \nIt includes twelve countries: Argentina, Bolivia, Brazil,  \nChile, Colombia, Ecuador, Guyana, Paraguay, Peru, Suriname,  \nUruguay, and Venezuela. The South American nations that  \nborder the Caribbean Sea—including Colombia, Venezuela,  \nGuyana, Suriname, as well as French Guiana, which is an  \noverseas region of France—are also known as Caribbean South  \nAmerica. South America has an area of 17,840,000 square  \nkilometers (6,890,000 sq mi). Its population as of 2005  \nhas been estimated at more than 371,090,000. South America  \nranks fourth in area (after Asia, Africa, and North America)  \nand fifth in population (after Asia, Africa, Europe, and  \nNorth America). The word America was coined in 1507 by  \ncartographers Martin Waldseemüller and Matthias Ringmann,  \nafter Amerigo Vespucci, who was the first European to  \nsuggest that the lands newly discovered by Europeans were  \nnot India, but a New World unknown to Europeans.",
-    "surface": 17840000,
+const data = {
+    "name": "South America",
+    "summary": "South America (Spanish: América del Sur, Sudamérica or  \nSuramérica; Portuguese: América do Sul; Quechua and Aymara:  \nUrin Awya Yala; Guarani: Ñembyamérika; Dutch: Zuid-Amerika;  \nFrench: Amérique du Sud) is a continent situated in the  \nWestern Hemisphere, mostly in the Southern Hemisphere, with  \na relatively small portion in the Northern Hemisphere.  \nThe continent is also considered a subcontinent of the  \nAmericas.[2][3] It is bordered on the west by the Pacific  \nOcean and on the north and east by the Atlantic Ocean;  \nNorth America and the Caribbean Sea lie to the northwest.  \nIt includes twelve countries: Argentina, Bolivia, Brasil,  \nChile, Colombia, Ecuador, Guyana, Paraguay, Peru, Suriname,  \nUruguay, and Venezuela. The South American nations that  \nborder the Caribbean Sea—including Colombia, Venezuela,  \nGuyana, Suriname, as well as French Guiana, which is an  \noverseas region of France—are a.k.a. Caribbean South  \nAmerica. South America has an area of 17,840,000 square  \nkilometers (6,890,000 sq mi). Its population as of 2005  \nhas been estimated at more than 371,090,000. South America  \nranks fourth in area (after Asia, Africa, and North America)  \nand fifth in population (after Asia, Africa, Europe, and  \nNorth America). The word America was coined in 1507 by  \ncartographers Martin Waldseemüller and Matthias Ringmann,  \nafter Amerigo Vespucci, who was the first European to  \nsuggest that the lands newly discovered by Europeans were  \nnot India, but a New World unknown to Europeans.",
     "timezone": [
       -4,
       -2
     ],
     "demographics": {
-      "population": 385742554,
+      "population": 385744896,
       "largestCities": [
         "São Paulo",
         "Buenos Aires",
@@ -20,7 +18,7 @@ const data =
     "languages": [
       "spanish",
       "portuguese",
-      "english",
+      "inglés",
       "dutch",
       "french",
       "quechua",
@@ -31,7 +29,7 @@ const data =
     "countries": [
       {
         "name": "Argentina",
-        "capital": "Buenos Aires",
+        "capital": "Rawson",
         "independence": "1816-07-08T18:06:32.000Z",
         "unasur": true
       },
@@ -39,6 +37,12 @@ const data =
         "name": "Bolivia",
         "capital": "La Paz",
         "independence": "1825-08-05T18:06:32.000Z",
+        "unasur": true
+      },
+      {
+        "name": "Peru",
+        "capital": "Lima",
+        "independence": "1821-07-27T18:06:32.000Z",
         "unasur": true
       },
       {
@@ -51,12 +55,6 @@ const data =
         "name": "Chile",
         "capital": "Santiago",
         "independence": "1818-02-11T18:06:32.000Z",
-        "unasur": true
-      },
-      {
-        "name": "Colombia",
-        "capital": "Bogotá",
-        "independence": "1810-07-19T18:06:32.000Z",
         "unasur": true
       },
       {
@@ -78,28 +76,22 @@ const data =
         "unasur": true
       },
       {
-        "name": "Peru",
-        "capital": "Lima",
-        "independence": "1821-07-27T18:06:32.000Z",
-        "unasur": true
-      },
-      {
         "name": "Suriname",
         "capital": "Paramaribo",
         "independence": "1975-11-24T18:30:00.000Z",
         "unasur": true
       },
       {
-        "name": "Uruguay",
-        "capital": "Montevideo",
-        "independence": "1825-08-24T18:06:32.000Z",
-        "unasur": true
+        "name": "Antártida",
+        "unasur": false
       },
       {
-        "name": "Venezuela",
-        "capital": "Caracas",
-        "independence": "1811-07-04T18:06:32.000Z",
-        "unasur": true
+        "name": "Colombia",
+        "capital": "Bogotá",
+        "independence": "1810-07-19T18:06:32.000Z",
+        "unasur": true,
+        "population": 42888594
       }
-    ]
+    ],
+    "spanishName": "Sudamérica"
   }
